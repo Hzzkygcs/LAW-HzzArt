5d066e422d23e518e7edd0bed5db022c397027bd
Tara Mazaya Lababan <tara.mazaya@ui.ac.id>
2023-04-21 21:49:41 +0700

    feat(progress): collection orchestration

--- collection-interactions-orchestration/modules/util/like-rating-collections/get-popular-collections.js
+++ collection-interactions-orchestration/modules/util/like-rating-collections/get-popular-collections.js
@@ -1,18 +1,14 @@
-const axios = require('axios');
-const {getArtCollectionsUrl} = require('../../../URLs/get-like-rating-service-url');
+const {getLikeRatingServiceUrl} = require('../../../URLs/get-like-rating-service-url');
+const {makeRequest} = require("../../external-call/make-request");
 
 async function getPopularCollections(search) {
-  const url = getArtCollectionsUrl('/get-like-rating-collections');
-  response = await axios.get(url, {
+  const url = getLikeRatingServiceUrl('/get-like-rating-collections');
+  let response = await makeRequest("get", url, {
     params: {
-        search: search
+        "search": search
     }
-    }).then((response) => {
-        return response.data;
-    }).catch((error) => {
-        console.log(error);
-    });
-    return response;
+  })
+    return response.data;
 }
 
 module.exports.getPopularCollections = getPopularCollections;
--- collection-interactions-orchestration/modules/util/like-rating-collections/send-like-service.js
+++ collection-interactions-orchestration/modules/util/like-rating-collections/send-like-service.js
@@ -1,18 +1,14 @@
-const axios = require("axios");
 const {getLikeRatingServiceUrl} = require("../../../URLs/get-like-rating-service-url");
+const {makeRequest} = require("../../external-call/make-request");
 
 async function sendLikeService(username, collectionId, like) {
     const url = getLikeRatingServiceUrl("/like-collection");
-    const response = await axios.post(url, {
-        username: username,
-        collectionId: collectionId,
-        like: like
-    }).then((response) => {
-        console.log(response);
-    }).catch((error) => {
-        console.log(error);
-    });
-    return response;
+    let response = await makeRequest("post", url, {
+        "username": username,
+        "collectionId": collectionId,
+        "like": like
+    })
+    return response.data;
 }
 
 module.exports.sendLikeService = sendLikeService;
