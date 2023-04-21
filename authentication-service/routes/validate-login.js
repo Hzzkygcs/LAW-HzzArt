2eff1fb21c55ad429c6d49d64ca65053c810958b
Immanuel01 <immanuel01@ui.ac.id>
2023-04-21 19:24:37 +0700

    fix: Error (500) on username not found

--- authentication-service/routes/validate-login.js
+++ authentication-service/routes/validate-login.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {getUserJoiValidation, User} = require("../model/user");
+const {getUserJoiValidation, User, getOneUserOrThrow} = require("../model/user");
 const {UsernameNotFoundException} = require("../modules/exceptions/UsernameNotFoundException");
 
 const route = express.Router();
@@ -8,13 +8,8 @@ route.post("/", async (req, res) => {
     const validate = getUserJoiValidation(["username", "password"]);
     validate(req.body);
 
-    const users = await User.find({username: req.body.username});
-    if (users.length === 0)
-        throw new UsernameNotFoundException(`Username ${req.body.username} is not found`);
-
-    let user = users[0];
+    let user = await getOneUserOrThrow(req.body.username);
     await user.validatePasswordAsync(req.body.password);
-
     user = user.getObjectRepresentation();
     res.send(user);
 })
