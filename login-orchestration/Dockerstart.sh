d1f679113ef86099f1db688e78fcb4adf1ada772
Immanuel01 <immanuel01@ui.ac.id>
2023-05-01 23:59:20 +0700

    chore: remove problematic Dockerstart.sh
    Cannot run, echo syntax error on GCP instance
    
    Ran correctly on my computer
    
    Couldnt run on valen's PC, package.json not updated when the package.json is not cat-ed. see 5b12eb5ca7f434053517acd48e3bec81c60e9898

--- login-orchestration/Dockerstart.sh
+++
@@ -1,6 +0,0 @@
-echo running
-echo $(pwd)
-echo $(cat package.json)  # magic spell biar jalan di laptop valen
-echo $(ls -a)             # in other words to update package.json
-npm run nodemon_run_legacy
-
