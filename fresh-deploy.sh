658f864be8488998fcd28e7b995f9ca6e030d4b9
Immanuel01 <immanuel01@ui.ac.id>
2023-05-01 23:55:13 +0700

    chore: edit fresh-deploy.sh

--- fresh-deploy.sh
+++ fresh-deploy.sh
@@ -1,4 +1,6 @@
 sudo docker-compose down --volumes --rmi
+sudo docker rm -vf $(docker ps -aq)
+sudo docker rmi -f $(docker images -aq)
 sudo docker-compose rm -f
 sudo docker-compose pull
 sudo docker-compose build --no-cache
