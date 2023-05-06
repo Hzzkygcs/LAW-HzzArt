478dc153185a6f75d64f0a89f3fec7c61e461eda
Ananda Fadhil <ananda.fadhil@ui.ac.id>
2023-05-06 22:17:44 +0700

    dockkkkkkkkkkkkkkkkkkeeeeeeeeeeeeeerrrrrrrrrrrrrrr

--- like_comment_management/like_comment/urls.py
+++ like_comment_management/like_comment/urls.py
@@ -6,11 +6,9 @@ from .views import *
 urlpatterns = [
     
     path('get-login-url', get_login_url, name='get_login_url'),
-    path('save-user', save_user, name='save_user'),
-    # path('create_user', create_user, name='create_user'),
-    path('create-post/<str:username>', create_post, name='create_post'),
+    path('get-user', get_user, name='get_user'),
     path('get-post/<int:post_id>', get_post, name='get_post'),
-    path('like/<int:post_id>/<str:username>', like, name='like'),
+    path('like/<int:post_id>', like, name='like'),
     path('liked', liked, name='liked'),
     path('comment/<int:post_id>/<str:username>', comment, name='comment'),
     path('get-comment/<int:post_id>', get_comment, name='get_comment'),
--- like_comment_management/like_comment/views.py
+++ like_comment_management/like_comment/views.py
@@ -31,10 +31,13 @@ def get_user(request):
         try:
             data = response.json()
             username = data['username']
+            customer = Cust(username=username)
+            customer.save()
             return username
         except json.decoder.JSONDecodeError as e:
             # Handle the JSONDecodeError
             return JsonResponse({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
     else:
         # Handle the HTTP error response
         return JsonResponse({"error": f"HTTP {response.status_code} error: {response.reason}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -44,56 +47,25 @@ def get_collection_id(request):
     return "http://localhost:8084"
 
 @csrf_exempt
-def get_collection(request):
-    data = response.json()
-    id = data['id']
-    name = data['name']
-    image = data['image']
-    owner = data['owner']
-    return id, name, image, owner
-
-# @csrf_exempt
-# def save_collection(request):
-#     # Send the token to the login orchestration service
-#     response = requests.post(get_collection_id() + '/login',body={'Authorization': f'Bearer {jwt_token}'})
-
-#     data = response.json()
-#     username = data['username']
-
-#     #save user
-#     user = Cust(username=username)
-#     user.save()
-#     return JsonResponse({"message": "User successfully saved"}, status=status.HTTP_200_OK)
-
-@csrf_exempt
-def create_post(request, username):
-    post = get_collection(request)
-    deserialize = json.loads(request.body)
-    post = Post(post_id=deserialize['id'], post_name=deserialize['name'],post_image_link=deserialize['image'], post_user_name=deserialize['owner'])
-    post.save()
-    return JsonResponse({"message": "Post created successfully"}, status=status.HTTP_201_CREATED)
-
-@csrf_exempt
 def get_post(request, post_id):
-    post = Post.objects.filter(post_id=post_id).values()[0]
+    post = Collection.objects.filter(post_id=post_id).values()[0]
     return JsonResponse({"response":post}, status=status.HTTP_200_OK)
 
 @csrf_exempt
 def like(request, post_id):
     user = get_user(request)
-    post = get_collection(request)
+    post = Collection.objects.get_or_create(post_id=post_id)
     like = Like.objects.filter(like_post=post, like_user=user)
-    collection = Post.objects.get(post_id=post)
     if not like.exists():
         add_like = Like(like_post=post, like_user=user)
         add_like.save()
-        collection.post_likes += 1
-        collection.save()
+        post.post_likes += 1
+        post.save()
         return JsonResponse({'message': 'Post Liked'}, status=status.HTTP_200_OK)
     else:
         like.delete()
-        collection.post_likes -= 1
-        collection.save()
+        post.post_likes -= 1
+        post.save()
         return JsonResponse({'message': 'Post Unliked'}, status=status.HTTP_200_OK)
     
 @csrf_exempt
