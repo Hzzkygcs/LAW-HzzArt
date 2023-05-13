58cc3004d8b3cf583b1ac4ede027568c9a3a5cd0
Immanuel01 <immanuel01@ui.ac.id>
2023-05-14 00:49:15 +0700

    chore(docker-compose): gatau

---
+++ art-collection-service/alembic_config.py
@@ -0,0 +1,22 @@
+"""
+not used
+"""
+
+import sys
+import os
+from configparser import ConfigParser
+
+# db_url = "postgresql://postgres:t9D56xfRPPqGdKids7I68O64glP5dKH4yUS5H52L2bnFMgHaHq@localhost:7000/ai-art-collections"
+
+if len(sys.argv) >= 2:
+    db_url = sys.argv[1]
+
+with open('alembic.ini', 'r') as f:
+    file_content = f.read()
+
+file_content = file_content.replace("{{{DATABASE_HOST_URL}}}", db_url)
+
+with open('alembic.ini', 'w') as f:
+    f.write(file_content)
+
+# raise Exception(repr(db_url))
