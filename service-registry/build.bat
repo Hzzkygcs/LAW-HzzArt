15fb33dc1713e2129b12b59472d4d66733114828
Immanuel01 <immanuel01@ui.ac.id>
2023-05-19 23:32:08 +0700

    refactor: remove unnecessary files and lines

--- service-registry/build.bat
+++
@@ -1,4 +0,0 @@
-docker image prune --force --filter="label=eureka-hzzart"
-docker build --tag eureka-hzzart:latest --label eureka-hzzart .
-docker rm -f eureka-hzzart
-docker run --name eureka-hzzart -p 8761:8761 eureka-hzzart
