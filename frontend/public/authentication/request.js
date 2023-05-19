0c22fc908ec302f2a716f70eb4015f572642aa64
Immanuel01 <immanuel01@ui.ac.id>
2023-05-19 18:22:02 +0700

    feat: Connect My Collections to art-collections-service

--- frontend/public/authentication/request.js
+++ frontend/public/authentication/request.js
@@ -1,8 +1,8 @@
-async function post(url, data, {isJson: isJson = true, contentType=null}={}){
+async function post(url, data, {isJson=true, contentType=null}={}){
     return request("POST", url, data, {isJson: isJson,contentType: contentType})
 }
-async function get(url, data, {isJson: isJson = true, contentType=null}){
-    return request("GET", url, data, {isJson: isJson,contentType: contentType})
+async function get(url, data, {contentType=null}={}){
+    return request("GET", url, data, {isJson: false,contentType: contentType})
 }
 
 
@@ -13,6 +13,9 @@ async function request(method, url, data, {isJson=true, contentType=null}){
             type: method,
             url: url,
             data: isJson ? JSON.stringify(data) : data,
+            headers: {
+              "x-jwt-token": localStorage.getItem("x-jwt-token"),
+            },
             contentType: contentType ?? (isJson ? 'application/json' : 'multipart/form-data'),
         }).done((data) => {
             data.validationFail = null;
