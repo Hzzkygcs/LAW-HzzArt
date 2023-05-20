7adffaeea71c98d097d093a9a24d416980672fca
Tara Mazaya Lababan <tara.mazaya@ui.ac.id>
2023-05-21 01:33:26 +0700

    progress popular collection

--- collection-interactions-orchestration/modules/util/collections/get-all-collections.js
+++ collection-interactions-orchestration/modules/util/collections/get-all-collections.js
@@ -1,10 +1,20 @@
-const {getArtCollectionsUrl} = require('../../../URLs/get-art-collections-url');
+const {getArtCollectionsUrl,getArtCollectionsLikeCommentUrl} = require('../../../URLs/get-art-collections-url');
 const {makeRequest} = require("../../external-call/make-request");
 
-async function getAllCollections() {
-  const url = getArtCollectionsUrl('/get-all-collections');
-  let response = await makeRequest("get", url)
+async function getAllCollections(token) {
+  const url = getArtCollectionsUrl('/collections-all');
+  let response = await makeRequest("get",
+    url,
+    {},
+    {"x-jwt-token": token}
+  );
+  return response.data;
+}
+
+async function getAllCollectionsWithLikeComments() {
+  const url = getArtCollectionsLikeCommentUrl('/like-comment/sort-collection/');
+  let response = await makeRequest("get", url, {}, {});
     return response.data;
 }
 
-module.exports.getAllCollections = getAllCollections;
+module.exports = {getAllCollections,getAllCollectionsWithLikeComments};
--- like_comment_service/like_comment_management/settings.py
+++ like_comment_service/like_comment_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4j#0=a1)dr2oqkzdwm7wgye!v+7mw#07y19b5vg@0tflddut-g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
