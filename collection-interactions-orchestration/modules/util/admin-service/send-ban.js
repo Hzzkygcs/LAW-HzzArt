65eafa824101358dbb508982a1e897b0708decb1
Tara Mazaya Lababan <tara.mazaya@ui.ac.id>
2023-05-20 17:37:14 +0700

    feat: implement button on detail report collection

---
+++ collection-interactions-orchestration/modules/util/admin-service/send-ban.js
@@ -0,0 +1,34 @@
+const {getAdminManagerUrl} = require("../../../URLs/get-admin-manager-url");
+const {makeRequest} = require("../../external-call/make-request");
+
+async function sendBanAccount(username) {
+    const url = getAdminManagerUrl("/admin/ban-account");
+    let response = await makeRequest("post",
+        url,
+        {
+        "username": username,
+        "isBan": true,
+        },
+        {
+            "x-service-token": process.env.ADMIN_SERVICE_TOKEN,
+        }
+    );
+    return response.data;
+}
+
+async function reportAcceptReject(collectionId,isBan) {
+    const url = getAdminManagerUrl("/admin/ban-collection");
+    let response = await makeRequest("post",
+        url,
+        {
+        "collectionId": collectionId,
+        "isBan": isBan,
+        },
+        {
+            "x-service-token": process.env.ADMIN_SERVICE_TOKEN,
+        }
+    );
+    return response.data;
+}
+
+module.exports = {sendBanAccount, reportAcceptReject};
