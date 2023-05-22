1aa22bed08c23da0929aae0b457c58e6edf34302
Immanuel01 <immanuel01@ui.ac.id>
2023-05-23 01:38:24 +0700

    chore(like-comment-service): Implement Consul initialization and fetch

--- like_comment_service/health/apps.py
+++ like_comment_service/health/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
 
-from consul_config.consul_services import initialize_consul
-
 
 class HealthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'health'
-    def ready(self):
-        initialize_consul()
